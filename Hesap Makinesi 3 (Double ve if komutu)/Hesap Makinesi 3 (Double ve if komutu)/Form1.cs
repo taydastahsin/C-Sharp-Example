c8be1be520e68fdd9650ecaa6a1e1d44959aa1d3
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-07-26 21:13:40 +0300

    Add files via upload

---
+++ Hesap Makinesi 3 (Double ve if komutu)/Hesap Makinesi 3 (Double ve if komutu)/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_Makinesi_3__Double_ve_if_komutu_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, toplam, çarpma, bölme, çıkarma;
+
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox3.Text);
+
+            toplam  = a + b;
+            çıkarma = a - b;
+            çarpma  = a * b;
+            bölme   = a / b;
+
+          if (textBox2  .Text == "+")
+            {
+                label5.Text = toplam.ToString();
+            }
+
+            if (textBox2.Text == "-")
+            {
+                label5.Text = çıkarma .ToString();
+            }
+
+            if (textBox2.Text == "*")
+            {
+                label5.Text = çarpma .ToString();
+            }
+
+            if (textBox2.Text == "/")
+            {
+                label5.Text = bölme .ToString();
+            }
+
+        }
+    }
+}
---
+++ Hesap Makinesi 3 (Double ve if komutu)/Hesap Makinesi 3 (Double ve if komutu)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_Makinesi_3__Double_ve_if_komutu_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
