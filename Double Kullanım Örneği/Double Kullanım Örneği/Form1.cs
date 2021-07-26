7eb4ad68972f53a7a79336f59ff9c98e4e48e421
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-07-26 19:34:25 +0300

    Add files via upload

---
+++ Double Kullanım Örneği/Double Kullanım Örneği/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Double_Kullanım_Örneği
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double  a;
+            double  b;
+            double  toplam;
+
+            a = Convert.ToDouble(textBox1 .Text);
+            b = Convert.ToDouble(textBox2.Text);
+
+            toplam = a + b;
+            label4.Text = toplam.ToString();
+
+
+        }
+    }
+}
