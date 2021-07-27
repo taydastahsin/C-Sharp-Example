01548997c8e1b9b77f89cc665697f7300f3fe54f
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-07-27 20:59:27 +0300

    Add files via upload

---
+++ FİBONACCİ Örneği/FİBONACCİ Örneği/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FİBONACCİ_Örneği
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayı1 = 1;
+            int sayı2 = 1;
+            int sayı3;
+
+            listBox1.Items.Add(sayı1);
+            listBox1.Items.Add(sayı2);
+
+            for (int i=0;i<=8;i++)
+            {
+                sayı3 = sayı1 + sayı2 ;
+
+                sayı1 = sayı2;
+                sayı2 = sayı3;
+
+                listBox1.Items.Add(sayı3);
+
+            }
+
+        }
+    }
+}
