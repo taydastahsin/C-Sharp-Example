df32594df7d7e7577f9f688c8452aaea15fa4ddb
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-08-10 21:41:04 +0300

    Add files via upload

---
+++ Taraftar Renk Timer/Taraftar Renk Timer/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace Taraftar_Renk_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 132);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 132);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(284, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 132);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(420, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 132);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 132);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(148, 150);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 132);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(284, 150);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(130, 132);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(420, 150);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(130, 132);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 650;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "0";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 322);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(39, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "FB";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(148, 322);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(41, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "GS";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(284, 322);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(45, 17);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "BJK";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(420, 322);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(40, 17);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "TS";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Sayaç :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Taraftar Renk Timer/Taraftar Renk Timer/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taraftar_Renk_Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sure = 0;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            button1.BackColor = Color.Yellow;
+            button2.BackColor = Color.Yellow;
+            button3.BackColor = Color.Blue ;
+            button4.BackColor = Color.Blue ;
+            button5.BackColor = Color.Yellow;
+            button6.BackColor = Color.Yellow;
+            button7.BackColor = Color.Blue ;
+            button8.BackColor = Color.Blue ;
+
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            button1.BackColor = Color.Yellow;
+            button2.BackColor = Color.Yellow;
+            button3.BackColor = Color.Red ;
+            button4.BackColor = Color.Red ;
+            button5.BackColor = Color.Yellow;
+            button6.BackColor = Color.Yellow;
+            button7.BackColor = Color.Red ;
+            button8.BackColor = Color.Red ;
+
+            checkBox1.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            button1.BackColor = Color.Black ;
+            button2.BackColor = Color.Black ;
+            button3.BackColor = Color.White ;
+            button4.BackColor = Color.White ;
+            button5.BackColor = Color.Black ;
+            button6.BackColor = Color.Black ;
+            button7.BackColor = Color.White ;
+            button8.BackColor = Color.White ;
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            button1.BackColor = Color.DarkViolet;
+            button2.BackColor = Color.DarkViolet;
+            button3.BackColor = Color.Blue;
+            button4.BackColor = Color.Blue;
+            button5.BackColor = Color.DarkViolet;
+            button6.BackColor = Color.DarkViolet;
+            button7.BackColor = Color.Blue;
+            button8.BackColor = Color.Blue;
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure = Convert.ToInt32(label1.Text);
+            sure++;
+            label1.Text = sure.ToString();
+
+            if (checkBox1.Checked == true)
+            {
+                if (sure % 10 == 0)
+                {
+                    button1.BackColor = Color.Yellow;
+                    button2.BackColor = Color.Blue;
+                    button3.BackColor = Color.Yellow;
+                    button4.BackColor = Color.Blue;
+                    button5.BackColor = Color.Yellow;
+                    button6.BackColor = Color.Blue;
+                    button7.BackColor = Color.Yellow;
+                    button8.BackColor = Color.Blue;
+
+                }
+
+                if (sure % 10 == 5)
+                {
+
+                    button1.BackColor = Color.Blue;
+                    button2.BackColor = Color.Yellow;
+                    button3.BackColor = Color.Blue;
+                    button4.BackColor = Color.Yellow;
+                    button5.BackColor = Color.Blue;
+                    button6.BackColor = Color.Yellow;
+                    button7.BackColor = Color.Blue;
+                    button8.BackColor = Color.Yellow;
+                }
+            }
+            if (checkBox2.Checked == true)
+            {
+                if (sure % 10 == 0)
+                {
+                    button1.BackColor = Color.Yellow;
+                    button2.BackColor = Color.Red ;
+                    button3.BackColor = Color.Yellow;
+                    button4.BackColor = Color.Red ;
+                    button5.BackColor = Color.Yellow;
+                    button6.BackColor = Color.Red ;
+                    button7.BackColor = Color.Yellow;
+                    button8.BackColor = Color.Red ;
+
+                }
+
+                if (sure % 10 == 5)
+                {
+
+                    button1.BackColor = Color.Red ;
+                    button2.BackColor = Color.Yellow;
+                    button3.BackColor = Color.Red ;
+                    button4.BackColor = Color.Yellow;
+                    button5.BackColor = Color.Red ;
+                    button6.BackColor = Color.Yellow;
+                    button7.BackColor = Color.Red ;
+                    button8.BackColor = Color.Yellow;
+                }
+            }
+            if (checkBox3.Checked == true)
+            {
+                if (sure % 10 == 0)
+                {
+                    button1.BackColor = Color.Black ;
+                    button2.BackColor = Color.White ;
+                    button3.BackColor = Color.Black ;
+                    button4.BackColor = Color.White  ;
+                    button5.BackColor = Color.Black ;
+                    button6.BackColor = Color.White ;
+                    button7.BackColor = Color.Black ;
+                    button8.BackColor = Color.White ;
+
+                }
+
+                if (sure % 10 == 5)
+                {
+
+                    button1.BackColor = Color.White ;
+                    button2.BackColor = Color.Black ;
+                    button3.BackColor = Color.White ;
+                    button4.BackColor = Color.Black ;
+                    button5.BackColor = Color.White ;
+                    button6.BackColor = Color.Black ;
+                    button7.BackColor = Color.White ;
+                    button8.BackColor = Color.Black ;
+                }
+            }
+            if (checkBox4.Checked == true)
+            {
+                if (sure % 10 == 0)
+                {
+                    button1.BackColor = Color.DarkViolet ;
+                    button2.BackColor = Color.Blue;
+                    button3.BackColor = Color.DarkViolet ;
+                    button4.BackColor = Color.Blue;
+                    button5.BackColor = Color.DarkViolet ;
+                    button6.BackColor = Color.Blue;
+                    button7.BackColor = Color.DarkViolet ;
+                    button8.BackColor = Color.Blue;
+
+                }
+
+                if (sure % 10 == 5)
+                {
+
+                    button1.BackColor = Color.Blue;
+                    button2.BackColor = Color.DarkViolet ;
+                    button3.BackColor = Color.Blue;
+                    button4.BackColor = Color.DarkViolet ;
+                    button5.BackColor = Color.Blue;
+                    button6.BackColor = Color.DarkViolet ;
+                    button7.BackColor = Color.Blue;
+                    button8.BackColor = Color.DarkViolet ;
+                }
+            }
+        }
+    }
+}
