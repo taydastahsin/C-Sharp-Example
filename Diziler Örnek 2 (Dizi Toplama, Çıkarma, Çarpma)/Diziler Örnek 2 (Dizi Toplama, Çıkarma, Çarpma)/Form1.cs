dac6b492642f42927ed0de3355eb7eefece9226c
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-08-24 21:19:57 +0300

    Add files via upload

---
+++ Diziler Örnek 2 (Dizi Toplama, Çıkarma, Çarpma)/Diziler Örnek 2 (Dizi Toplama, Çıkarma, Çarpma)/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_Örnek_2__Dizi_Toplama__Çıkarma__Çarpma_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayılar = { 3, 4, 5 };
+
+            int toplam = 0;
+            int çıkarma = 0;
+            int çarpma = 1;
+
+            for(int i=0;i < sayılar .Length;i++ )
+            {
+                toplam = toplam + sayılar[i];
+                label4.Text = toplam.ToString();
+
+                çıkarma  = çıkarma  - sayılar[i];
+                label5.Text = çıkarma .ToString();
+
+                çarpma  = çarpma  * sayılar[i];
+                label6.Text = çarpma .ToString();
+
+            }
+        }
+    }
+}
---
+++ Diziler Örnek 2 (Dizi Toplama, Çıkarma, Çarpma)/Diziler Örnek 2 (Dizi Toplama, Çıkarma, Çarpma)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_Örnek_2__Dizi_Toplama__Çıkarma__Çarpma_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
