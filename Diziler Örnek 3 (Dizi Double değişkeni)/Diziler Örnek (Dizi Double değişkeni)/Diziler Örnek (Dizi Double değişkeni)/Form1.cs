1246e03efac80883120712594a5a781bd31cc117
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-08-25 20:48:25 +0300

    Add files via upload

---
+++ Diziler Örnek 3 (Dizi Double değişkeni)/Diziler Örnek (Dizi Double değişkeni)/Diziler Örnek (Dizi Double değişkeni)/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_Örnek__Dizi_Double_değişkeni_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] sayılar = { 1.5, 2.3, 3.8, 4.5, 5.1, 6.9 };
+            int eleman = sayılar.Length;
+            label5.Text = eleman.ToString();
+
+            int sayı;
+            sayı = Convert.ToInt32(textBox1.Text);
+
+            label1.Text = sayılar[sayı].ToString();
+
+
+        }
+    }
+}
---
+++ Diziler Örnek 3 (Dizi Double değişkeni)/Diziler Örnek (Dizi Double değişkeni)/Diziler Örnek (Dizi Double değişkeni)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_Örnek__Dizi_Double_değişkeni_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
