0eaa7c3301bdfa57dd1934d01dcd43f7e1f35aa7
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-08-26 08:56:00 +0300

    Add files via upload

---
+++ Diziler Örnek 4 (Dizi ile Foreach kullanımı)/Diziler Örnek 4 (Dizi ile Foreach kullanımı)/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_Örnek_4__Dizi_ile_Foreach_kullanımı_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayı1 = Convert.ToInt32(textBox1.Text);
+            int sayı2 = Convert.ToInt32(textBox2.Text);
+            int toplam = 0;
+
+            int[] sayılar = { sayı1, sayı2 };
+
+            foreach (int sayı in sayılar)
+            {
+                toplam = toplam + sayı;
+                label4.Text = toplam.ToString();
+
+            }
+
+            
+        }
+    }
+}
