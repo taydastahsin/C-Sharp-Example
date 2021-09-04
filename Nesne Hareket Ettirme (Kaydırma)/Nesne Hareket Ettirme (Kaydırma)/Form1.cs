9968ed0725a24d0f09d2a3d4cf73eed14e6fabab
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-09-04 23:13:19 +0300

    Add files via upload

---
+++ Nesne Hareket Ettirme (Kaydırma)/Nesne Hareket Ettirme (Kaydırma)/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nesne_Hareket_Ettirme__Kaydırma_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+            if (e.KeyCode == Keys.Up)
+            {
+                y = y - 5;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                y = y + 5;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                x = x + 5;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                x = x - 5;
+            }
+
+            pictureBox1.Location = new Point(x, y);
+        }
+    }
+}
