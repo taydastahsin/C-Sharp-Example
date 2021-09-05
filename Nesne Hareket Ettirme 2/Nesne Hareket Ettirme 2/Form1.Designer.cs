2d298dbfae938c8ee9a84ef119177824c420ff18
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-09-05 21:18:29 +0300

    Add files via upload

---
+++ Nesne Hareket Ettirme 2/Nesne Hareket Ettirme 2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Nesne_Hareket_Ettirme_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 257);
+            this.label1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(313, 139);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(750, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 257);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(226, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(315, 34);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(223, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(318, 34);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tomato;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Nesne Hareket Ettirme 2/Nesne Hareket Ettirme 2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nesne_Hareket_Ettirme_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+            if (e.KeyCode ==Keys.Up )
+            {
+                y = y - 5;
+            }
+            if (e.KeyCode == Keys.Down  )
+            {
+                y = y + 5;
+            }
+            if (e.KeyCode == Keys.Right  )
+            {
+                x = x + 5;
+            }
+            if (e.KeyCode == Keys.Left )
+            {
+                x = x - 5;
+            }
+
+            pictureBox1.Location = new Point(x, y);
+
+            if (pictureBox1 .Right >= label2 .Left)
+            {
+                label2.BackColor = Color.Red;
+            }
+            if (pictureBox1.Left <= label1.Right )
+            {
+                label1.BackColor = Color.Blue ;
+            }
+            if (pictureBox1.Top  <= label4.Bottom )
+            {
+                label4.BackColor = Color.Green ;
+            }
+            if (pictureBox1.Bottom >= label3.Top )
+            {
+                label3.BackColor = Color.Yellow ;
+            }
+        }
+    }
+}
