1a7ee3f557884a5f363b5dab48fe90c979a8db82
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-05 12:41:06 +0300

    Add files via upload

---
+++ Veri Tabanında Arama (SQL Server)/Veri Tabanında Arama (SQL Server)/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Veri_Tabanında_Arama__SQL_Server_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Verileri Göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 205);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(380, 159);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ad";
+            this.columnHeader1.Width = 71;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Soyad";
+            this.columnHeader2.Width = 91;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Yaş";
+            this.columnHeader3.Width = 51;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "İlçe";
+            this.columnHeader4.Width = 70;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Meslek";
+            this.columnHeader5.Width = 92;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Bul";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(268, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Aranacak İsim :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(383, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 18);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 385);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Veri Tabanında Arama (SQL Server)/Veri Tabanında Arama (SQL Server)/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Veri_Tabanında_Arama__SQL_Server_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-0QQE4BG;Initial Catalog=Bilgiler;Integrated Security=True");
+        
+        private void verilerigöster()
+        {
+            baglan.Open();
+            listView1.Items.Clear();
+            SqlCommand komut = new SqlCommand("Select * from Kişiler",baglan );
+
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+
+                ekle.Text = oku["Ad"].ToString();
+                ekle.SubItems.Add(oku["Soyad"].ToString());
+                ekle.SubItems.Add(oku["Yaş"].ToString());
+                ekle.SubItems.Add(oku["İlçe"].ToString());
+                ekle.SubItems.Add(oku["Meslek"].ToString());
+
+                listView1.Items.Add(ekle );
+
+                
+            }
+            baglan.Close();
+
+        }
+
+        private void bul()
+        {
+            baglan.Open();
+
+
+            SqlCommand komut = new SqlCommand("Select * from Kişiler where Ad like '% " + textBox1.Text + "% ' ", baglan);
+
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+
+                ekle.Text = oku["Ad"].ToString();
+                ekle.SubItems.Add(oku["Soyad"].ToString());
+                ekle.SubItems.Add(oku["Yaş"].ToString());
+                ekle.SubItems.Add(oku["İlçe"].ToString());
+                ekle.SubItems.Add(oku["Meslek"].ToString());
+
+                listView1.Items.Add(ekle);
+
+
+            }
+            baglan.Close();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verilerigöster();
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bul();
+            listView1.Items.Clear();
+        }
+
+    }
+}
