572d165493ac2ebaba5615ba306ea37395669d6b
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-06 22:53:42 +0300

    Add files via upload

---
+++ DataGridview 1 (Sütün ekleme)/DataGridview 1 (Sütün ekleme)/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridview_1__Sütün_ekleme_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].Name = "SıraNo:";
+            dataGridView1.Columns[1].Name = "İsim";
+            dataGridView1.Columns[2].Name = "Soyad";
+            dataGridView1.Columns[3].Name = "Meslek";
+            dataGridView1.Columns[4].Name = "İl"; 
+        }
+    }
+}
