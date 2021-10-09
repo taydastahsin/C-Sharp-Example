504a0a8aa8566ac00d8d0ece2c00d1b6161faf3e
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-09 21:50:44 +0300

    Add files via upload

---
+++ DataGridview 1.3 (Sql tablosu ekleme)/DataGridview 1.3 (Sql tablosu ekleme)/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataGridview_1._3__Sql_tablosu_ekleme_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-0QQE4BG;Initial Catalog=Bilgiler;Integrated Security=True");
+
+        public void verilerigöster(String veri)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(veri, baglan);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verilerigöster("Select * from Kişiler");
+        }
+    }
+}
