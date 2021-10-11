967da306d391ba2f7b66003cf78366d5f304ff46
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-12 00:05:34 +0300

    Add files via upload

---
+++ DataGridview 3 (Ekleme Silme Güncelleme Arama)/DataGridview 3 (Ekleme Silme Güncelleme Arama)/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataGridview_3__Ekleme_Silme_Güncelleme_Arama_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-0QQE4BG;Initial Catalog=Kütüphane;Integrated Security=True");
+        
+        private void kitaplar(string veri)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(veri,baglan);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kitaplar("Select * From Kitaplar");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("insert into Kitaplar (id,Kitap,Yazar,Yayınevi,Sayfa) values (@idi,@kitapi,@yazari,@yayınyeri,@sayfası)",baglan );
+            komut.Parameters.AddWithValue ("@idi", textBox1.Text);
+            komut.Parameters.AddWithValue("@kitapi", textBox2.Text);
+            komut.Parameters.AddWithValue("@yazari", textBox3.Text);
+            komut.Parameters.AddWithValue("@yayınyeri", textBox4.Text);
+            komut.Parameters.AddWithValue("@sayfası", textBox5.Text);
+
+            komut.ExecuteNonQuery();
+            kitaplar("Select * from Kitaplar");
+            baglan.Close();
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            
+        }
+        int a = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+            a++;
+            if (a == 1)
+            {
+                MessageBox.Show("Lütfen silmek  istediğiniz kitabın sayısını giriniz!!");
+            }
+            
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("delete from Kitaplar where id = @idi",baglan );
+            komut.Parameters.AddWithValue("@idi", textBox1.Text);
+            komut.ExecuteNonQuery();
+            kitaplar("Select * from Kitaplar");
+            baglan.Close();
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+        }
+        int b = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            b++;
+            if (b == 1)
+            {
+                MessageBox.Show("Lütfen aramak  istediğiniz kitabın adını giriniz!!");
+            }
+
+            baglan.Open();
+
+            SqlCommand komut = new SqlCommand("Select * from Kitaplar where Kitap like '%"+textBox2 .Text +"%'",baglan );
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+            baglan.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DataGridview 3 (Ekleme Silme Güncelleme Arama)/DataGridview 3 (Ekleme Silme Güncelleme Arama)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataGridview 3 (Ekleme Silme Güncelleme Arama)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataGridview 3 (Ekleme Silme Güncelleme Arama)")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e2da434-d12e-4c50-b057-867223b684fb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
